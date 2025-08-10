655617c0fb9660df4af4ab7743b9bc45cafd1cc7
Zspolanka <zspolanka@Zspolanka--MacBook-Pro.local>
2025-08-10 23:57:11 +0200

    Oprava unused importu

--- canary/app/services/page.tsx
+++ canary/app/services/page.tsx
@@ -1,12 +1,1 @@
-import Image from "next/image";
-import Navbar from "../components/Navbar";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col min-h-screen relative">
-      <Navbar />
-      <div className="absolute top-0 left-0 w-full h-full">
-      </div>
-    </div>
-  );
-}
+//LOL
