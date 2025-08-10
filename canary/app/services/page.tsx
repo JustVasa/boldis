8b6071428c44ffaf2b38608d57c4fb747f9dfbff
Zspolanka <zspolanka@Zspolanka--MacBook-Pro.local>
2025-08-10 23:39:42 +0200

    Aktualizace webu

--- canary/app/services/page.tsx
+++ canary/app/services/page.tsx
@@ -6,16 +6,6 @@ export default function Home() {
     <div className="flex flex-col min-h-screen relative">
       <Navbar />
       <div className="absolute top-0 left-0 w-full h-full">
-        <Image
-          src="/background4.png"
-          alt="Cleaning service background"
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-        />
-      </div>
-      <div className="relative z-10 flex-grow bg-white bg-opacity-10">
-        {/* Obsah stránky zde */}
       </div>
     </div>
   );
